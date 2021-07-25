1dea640d7fb10852907be302151de284cab2bf0a
sabreenMohammed107 <sabreenm312@gmail.com>
2021-07-25 23:20:16 +0200

    test

--- resources/views/web/single-blog.blade.php
+++ resources/views/web/single-blog.blade.php
@@ -40,16 +40,16 @@
 
 $blogId = $blog->id;
 if( LaravelLocalization::getCurrentLocale() === "en"){
-    $url = 'http://premierupvc.senior-consultingco.com/en/single-blog/'. $blogId;
+    $url = 'http://premierupvc.senior-consultingco.com/en/single-blog/'.$blogId;
 }else{
-    $url = 'http://premierupvc.senior-consultingco.com/ar/single-blog/'. $blogId;
+    $url = 'http://premierupvc.senior-consultingco.com/ar/single-blog/'.$blogId;
 }
 						 
 
 ?>
 							<ul class="ftco-footer-social ftco-blog-social list-unstyled {{ LaravelLocalization::getCurrentLocale() === "ar" ? 'float-md-right float-rtl' : ''}}">
 								<li class="ftco-animate"><a <a href="https://twitter.com/intent/tweet?url={{ urlencode($url) }}"  target="popup" onclick="window.open('https://twitter.com/intent/tweet?url={{ urlencode($url) }}','popup','width=600,height=600'); return false;" class="border-white"><span class="icon-twitter"></span></a></li>
-								<li class="ftco-animate"><a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}','popup','width=600,height=600'); return false;" class="border-white"><span class="icon-facebook"></span></a></li>
+								<li class="ftco-animate"><a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u={{urlencode($url)}}','popup','width=600,height=600'); return false;" class="border-white"><span class="icon-facebook"></span></a></li>
 								<li class="ftco-animate"><a
                                 href="https://www.instagram.com/sharer/sharer.php?u={{ urlencode($url) }}" target="popup" onclick="window.open('https://www.instagram.com/sharer/sharer.php?u={{ urlencode($url) }}','popup','width=600,height=600'); return false;"
                                  class="border-white"><span class="icon-instagram"></span></a></li>
