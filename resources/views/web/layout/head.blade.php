1a035be56820ba809b5ebd2f033bdbf188956666
sabreenMohammed107 <sabreenm312@gmail.com>
2021-07-26 13:57:43 +0200

    tes res

--- resources/views/web/layout/head.blade.php
+++ resources/views/web/layout/head.blade.php
@@ -3,7 +3,8 @@
   <head>
     <title>Premier UPVC</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://k1ngzed.com/dist/swiper/swiper.min.css">
